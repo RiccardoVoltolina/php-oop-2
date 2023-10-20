31d42932e83c08859c37fa2bc8444e6d40762346
Riccardo <riky.volto@live.com>
2023-10-20 16:20:47 +0200

    create macro class and sons class

--- index.php
+++ index.php
@@ -8,4 +8,53 @@ Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, 
 Bonus (non opzionale):
 organizzate il progetto come visto stamattina a lezione usando varie sottocartelle per inserire classi, layout e dati. */
 
+class Prodotti
+{
+    public $cibo;
+    public $giochi;
+    public $cucce;
+    public $prezzo;
+    public $titolo;
+    public $immagine;
+
+    public function __construct($cibo, $giochi, $cucce, $prezzo, $titolo, $immagine) {
+
+        $this->cibo = $cibo;
+        $this->giochi = $giochi;
+        $this->cucce = $cucce;
+        $this->prezzo = $prezzo;
+        $this->titolo = $titolo;
+        $this->immagine = $immagine;
+    }
+}
+
+$e_commerce = new prodotti('cibo per cani', 'pallina', 'cucce per cani', '30 euro', 'e-commerce', '');
+
+$prodottiArray = array($e_commerce);
+
+class Cani extends prodotti {
+
+}
+
+class Gatti extends prodotti {
+    
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach ($prodottiArray as $prodotto) :?>
+    <div>
+        <?php echo $prodotto->cibo?>
+
+    </div>
+    <?php endforeach; ?>
+</body>
+</html>
